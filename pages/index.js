abedd98ae5b1a95103ec984f6725622cede3f56b
Emma Waddell <52298111+reniby@users.noreply.github.com>
2023-06-09 19:09:32 -0400

    add project cards and nav bar

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,54 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+const projects = [
+  {
+      link: 'google.com',
+      title: 'Bird Ring',
+      descr: 'bird ring blah blah blah blah'
+  },
+  {
+      link: 'google.com',
+      title: 'Honors Thesis',
+      descr: 'bird ring blah blah blah blah'
+  },
+  {
+    link: 'google.com',
+    title: 'Video Game',
+    descr: 'bird ring blah blah blah blah'
+  },
+  {
+      link: 'google.com',
+      title: 'Ctrl 0',
+      descr: 'bird ring blah blah blah blah'
+  },
+  {
+    link: 'google.com',
+    title: 'Extractive Summ',
+    descr: 'bird ring blah blah blah blah'
+  },
+  {
+    link: 'google.com',
+    title: 'Neuro Circuits',
+    descr: 'bird ring blah blah blah blah'
+  },
+  {
+    link: 'google.com',
+    title: 'Stravinsky Stein',
+    descr: 'bird ring blah blah blah blah'
+  },
+  {
+      link: 'google.com',
+      title: 'HJS Contest',
+      descr: 'bird ring blah blah blah blah'
+  },
+  {
+    link: 'google.com',
+    title: 'Tug of Words',
+    descr: 'bird ring blah blah blah blah'
+  },
+]
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,41 +59,25 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Ori Raskin
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div className={styles.grid}>
+          <a href={'google.com'} className={styles.nav} key={'Projects'}>
+            <p>Projects</p>
+          </a>
+          <a href={'google.com'} className={styles.nav} key={'Projects'}>
+            <p>About</p>
+          </a>
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {projects.map((p) => (
+            <a href={p.link} className={styles.card} key={p.title}>
+              <h3>{p.title} &rarr;</h3>
+              <p>{p.descr}</p>
+            </a>
+          ))}
         </div>
       </main>
 
